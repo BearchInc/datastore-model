d0fe4cdf89ea381e57c3263c7f8b18e5dd342a6f
Heck/Marcon <heckfer@gmail.com>
2015-06-22 08:41:07 -0300

    [#90] Fix the iterator

--- page_iterator.go
+++ page_iterator.go
@@ -7,74 +7,70 @@ import (
 )
 
 type PageIterator struct {
-	query              *datastore.Query
-	context            appengine.Context
-	nextCursor         datastore.Cursor
-	prevCursor         datastore.Cursor
-	doneProcessingPage bool
+	query   *datastore.Query
+	context appengine.Context
+	nextCursor  datastore.Cursor
+	prevCursor datastore.Cursor
 }
 
 func NewPagesIterator(q *datastore.Query, c appengine.Context) *PageIterator {
 	return &PageIterator{
-		query:              q,
-		context:            c,
-		nextCursor:         datastore.Cursor{},
-		prevCursor:         datastore.Cursor{},
-		doneProcessingPage: false,
+		query:   q,
+		context: c,
 	}
 }
 
 // TODO refactor this mess :~
-// Perhaps it would be better to have a PerPageIterator and a PerItemIterator
-// to avoid messing up with the iterator internal state when using
-// LoadNext and LoadNextPage intermittently
 func (this *PageIterator) LoadNext(slice interface{}) error {
 	sv := reflect.ValueOf(slice)
 	if sv.Kind() != reflect.Ptr || sv.IsNil() || sv.Elem().Kind() != reflect.Slice {
 		return datastore.ErrInvalidEntityType
 	}
 	sv = sv.Elem()
-
 	elemType := sv.Type().Elem()
 	if elemType.Kind() != reflect.Ptr || elemType.Elem().Kind() != reflect.Struct {
 		return datastore.ErrInvalidEntityType
 	}
-
 	iter := this.query.Run(this.context)
 	for {
+		this.context.Infof("Epa epa epa 0")
 		dstValue := reflect.New(elemType.Elem())
 		dst := dstValue.Interface()
+
 		entity, ok := dst.(Entity)
 		if !ok {
+			this.context.Infof("Epa epa epa 1")
 			return datastore.ErrInvalidEntityType
 		}
 
 		key, err := iter.Next(entity)
-		this.prevCursor = this.nextCursor
-
-		cursor, cursorErr:= iter.Cursor()
-		if cursorErr != nil {
-			return cursorErr
-		}
-		this.nextCursor = cursor
-		entity.SetKey(key)
-		sv.Set(reflect.Append(sv, dstValue))
-
 		if err == datastore.Done {
-			this.doneProcessingPage = true
-			this.query = this.query.Start(cursor)
+			cursor, cursorErr := iter.Cursor()
+			if cursorErr != nil {
+				this.context.Infof("Epa epa epa 2")
+				return cursorErr
+			}
+			this.prevCursor = this.nextCursor
+			this.nextCursor = cursor
+			this.query = this.query.Start(this.nextCursor)
+			this.context.Infof("Epa epa epa 3")
 			return err
 		}
+
 		if err != nil {
-			return nil
+			this.context.Infof("Epa epa epa 4 - %+v", err)
+			return err
 		}
+
+		entity.SetKey(key)
+		sv.Set(reflect.Append(sv, dstValue))
 	}
 
 	return nil
 }
 
 func (this *PageIterator) HasNext() bool {
-	return !this.doneProcessingPage || this.prevCursor != this.nextCursor
+	return this.prevCursor.String() != this.nextCursor.String()
 }
 
 func (this *PageIterator) Cursor() string {
